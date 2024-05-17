018816b69744885bd8ac5796a7338c1165e00c54
Phillip Bement <phillipb@pbement.com>
2024-05-16 17:41:04 -0700

    add self-avoidance via markov chain monte carlo optimization. this is mandatory for us to be able to generate long chains. it does significantly increase the runtime, however. it's then easy to add some optimization for compactness, so we also do that. finally, we add proper termination (COO) for the last amino acid in the chain

--- pdb_util.py
+++ pdb_util.py
@@ -36,4 +36,34 @@ def write_xyz(fnm, atoms):
             f.write("%s   %f   %f   %f\n" % (atom.element, atom.pos[0], atom.pos[1], atom.pos[2]))
 
 
+def leftpad(s, n):
+    return " "*(n-len(s)) + s
+
+def pdb_name(s):
+    return " " + s + " "*(3-len(s))
+
+def pdb_float(f):
+    return "%8.3f" % f
+
+def pdb_line(atom, i_atom, i_seq, residue):
+    """ based on this: https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/tutorials/pdbintro.html """
+    return ("ATOM  "                # 1-6
+        + leftpad(str(i_atom), 5)   # 7-11
+        + " "                       # 12
+        + pdb_name(atom.name)       # 13-16
+        + " "                       # 17
+        + residue                   # 18-20
+        + " A"                      # 21-22
+        + leftpad(str(i_seq), 4)    # 23-26
+        + "    "                    # 27-30
+        + pdb_float(atom.pos[0])    # 31-38
+        + pdb_float(atom.pos[1])    # 39-46
+        + pdb_float(atom.pos[2])    # 47-54
+        + "  1.00"                  # 55-60
+        + "100.00"                  # 61-66
+        + "          "              # 67-76
+        + leftpad(atom.element, 2)  # 77-78
+        + "  "                      # 79-80
+    )
+
 
--- seq2pdbchain.py
+++ seq2pdbchain.py
@@ -1,59 +1,123 @@
+import sys
 import numpy as np
 
-from amino_data import letter_code, structures
+from amino_data import letter_code, structures, Atom
+from pdb_util import pdb_line
+from structures_codegen import extract_atom_positions
 
 
+# Maximum distance from any atom in an amino to the peptide nitrogen
+MAX_DIST_TO_N = 10.0              # [Å] rounded up from 8.48
+ATOM_RADIUS = 0.68                # [Å] atom radius for C, N, O
+COLLISION_DIST = 2.6*ATOM_RADIUS  # [Å] twice atom radius to form a bond, plus 0.6 safety margin
+COMPACTNESS_ITERATIONS = 200      # number of iterations to spend optimizing compactness
 
-def leftpad(s, n):
-    return " "*(n-len(s)) + s
-
-def pdb_name(s):
-    return " " + s + " "*(3-len(s))
-
-def pdb_float(f):
-    return "%8.3f" % f
-
-def pdb_line(atom, i_atom, i_seq, residue):
-    """ based on this: https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/tutorials/pdbintro.html """
-    return ("ATOM  "                # 1-6
-        + leftpad(str(i_atom), 5)   # 7-11
-        + " "                       # 12
-        + pdb_name(atom.name)       # 13-16
-        + " "                       # 17
-        + residue                   # 18-20
-        + " A"                      # 21-22
-        + leftpad(str(i_seq), 4)    # 23-26
-        + "    "                    # 27-30
-        + pdb_float(atom.pos[0])    # 31-38
-        + pdb_float(atom.pos[1])    # 39-46
-        + pdb_float(atom.pos[2])    # 47-54
-        + "  1.00"                  # 55-60
-        + "100.00"                  # 61-66
-        + "          "              # 67-76
-        + leftpad(atom.element, 2)  # 77-78
-        + "  "                      # 79-80
-    )
+def BETA(n_collisions): # [1/collisions] thermodynamic coolness for Monte Carlo method
+    return max(0.2, 10/n_collisions)
 
 
-
-def pdb_chain(sequence):
+def blks_to_pdb(residues, blks):
     lines = []
+    i_atom = 0
+    for i_seq, (residue, blk) in enumerate(zip(residues, blks)):
+        for atom in blk:
+            lines.append(pdb_line(atom, i_atom, 1 + i_seq, residue))
+            i_atom += 1
+    lines.append("TER " + pdb_line(Atom(" ", [0., 0., 0.], " "), i_atom, len(residues), residue)[4:27])
+    return "\n".join(lines)
+
+
+def amino_choices_to_blks(structs):
+    blks = []
     cursor = np.array([0., 0., 0.])
     transform = np.identity(3)
-    i_atom = 0
-    for i_seq, c in enumerate(sequence):
-        residue = letter_code[c]
-        struct = structures[residue][np.random.randint(len(structures[residue]))]
+    for struct in structs:
+        blk = []
         for atom in struct.atoms:
             transformed_atom = atom._replace(pos = cursor + (transform @ atom.pos))
             if atom.name != "N_next":
-                lines.append(pdb_line(transformed_atom, i_atom, i_seq, residue))
-                i_atom += 1
+                blk.append(transformed_atom)
             else:
                 cursor = transformed_atom.pos
                 break # NOTE: N_next should be the last element in the list!
         transform = transform @ struct.transform
-    return "\n".join(lines)
+        blks.append(blk)
+    # do a proper OC1, OC2 termination of the peptide
+    (CA_pos, C_pos, O_pos) = extract_atom_positions(["CA", "C", "O"], blks[-1])
+    OC2_pos = 3*C_pos - (CA_pos + O_pos)
+    idx_O = [i for i in range(len(blks[-1])) if blks[-1][i].name == "O"][0]
+    blks[-1][idx_O] = blks[-1][idx_O]._replace(name = "OC1")
+    blks[-1].append(Atom("OC2", OC2_pos, "O"))
+    return blks
+
+
+def count_collisions(blks):
+    blks = [ # convert each block into an (n, 3) numpy array for faster processing
+        np.stack([atom.pos for atom in blk])
+        for blk in blks]
+    collisions = 0
+    for i in range(len(blks)):
+        for j in range(i - 1): # directly adjacent aminos are assumed to be okay
+            blk_i, blk_j = blks[i][None, :], blks[j][:, None]
+            if np.linalg.norm(blk_i[0, 0] - blk_j[0, 0]) > 2*MAX_DIST_TO_N:
+                continue # guaranteed no collisions thanks to triangle inequality
+            collisions += (np.linalg.norm(blk_i - blk_j, axis=2) < COLLISION_DIST).sum()
+    return collisions
+
+
+def rough_radius(blks):
+    pos_sum = np.array([0., 0., 0.])
+    n = 0
+    for blk in blks:
+        for atom in blk:
+            pos_sum += atom.pos
+            n += 1
+    pos_avg = pos_sum / n
+    return max([np.linalg.norm(atom.pos - pos_avg)
+        for blk in blks for atom in blk])
+
+
+def pick_random_struct(residue):
+    choices = structures[residue]
+    return choices[np.random.randint(len(choices))]
+
+
+def pdb_chain(sequence, showprogress=True):
+    structs = []
+    residues = []
+    for c in sequence:
+        res = letter_code[c]
+        residues.append(res)
+        structs.append(pick_random_struct(res))
+    def mutate(struct_list):
+        ans = [struct for struct in struct_list]
+        j = np.random.randint(len(residues) - 2)
+        ans[j] = pick_random_struct(residues[j])
+        ans[j + 1] = pick_random_struct(residues[j + 1])
+        ans[j + 2] = pick_random_struct(residues[j + 2])
+        return ans
+    n_collisions = 1000000000000
+    while n_collisions > 0:
+        new_structs = mutate(structs)
+        blks = amino_choices_to_blks(new_structs)
+        new_n_collisions = count_collisions(blks)
+        # markov chain monte-carlo step:
+        if np.exp(BETA(n_collisions)*(n_collisions - new_n_collisions)) > np.random.rand():
+            n_collisions = new_n_collisions
+            structs = new_structs
+            if showprogress:
+                sys.stderr.write("current collisions: %d\n" % n_collisions)
+    radius = rough_radius(blks)
+    for i in range(COMPACTNESS_ITERATIONS):
+        new_structs = mutate(structs)
+        blks = amino_choices_to_blks(new_structs)
+        new_radius = rough_radius(blks)
+        if new_radius <= radius and count_collisions(blks) == 0:
+            radius = new_radius
+            structs = new_structs
+            if showprogress:
+                sys.stderr.write("current radius: %f\n" % radius)
+    return blks_to_pdb(residues, blks)
 
 
 if __name__ == "__main__":
